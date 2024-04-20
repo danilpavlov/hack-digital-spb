0b3a4cf538587bedd3f16a9ca938abb8c2a0e281
evildre <musaew97@gmail.com>
2024-04-21 00:06:00 +0300

    jwjwj

--- backend/src/filter/parser.py
+++ backend/src/filter/parser.py
@@ -38,4 +38,3 @@ def parse_etu():
         }
     )
     profs_df.to_csv('./backend/src/filter/data/prof.csv')
-
--- backend/src/filter/router.py
+++ backend/src/filter/router.py
@@ -1,21 +1,26 @@
 import os
+from flask import Flask, request, jsonify
 from parser import parse_etu
 from filter_wrapper import FilterWrapper
 
+app = Flask(__name__)
+
 data_url = 'backend/src/filter/data/Данные приёмной кампании.csv'
 profs_url = 'backend/src/filter/data/prof.csv'
-
 
 if not os.path.exists(profs_url):
     parse_etu()
 
 filter_wrap = FilterWrapper(data_url, profs_url)
 
-test_atr = {
-    'subjects': ['Математика', 'Информатика', 'Русский язык'],
-    'scores': [100, 100, 100],
+@app.route('/', methods=['GET'])
+def get_documents():
+    if request.method == 'GET' and request.is_json:
+        abiture_data = request.json
+        documents = filter_wrap.get_documents(abiture_data)
+        return jsonify(documents)
+    else:
+        return 'Invalid request', 400
 
-    'profession': ['Разработчик компьютерных программ (программист)', 'Тимлид']  # Желаемая профессия'
-}
-
-print(filter_wrap.get_documents(test_atr))
+if __name__ == '__main__':
+    app.run(port=5001)
