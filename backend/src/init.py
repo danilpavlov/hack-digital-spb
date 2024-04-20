1fe22720d218eaa427c80fc6ac150aa9f6fa76a9
evildre <musaew97@gmail.com>
2024-04-20 14:31:18 +0300

    jopa

--- backend/src/init.py
+++
@@ -1,7 +0,0 @@
-from src.config import settings
-from src.http_client import CMCHTTPClient
-
-cmc_client = CMCHTTPClient(
-    base_url='https://pro-api.coinmarketcap.com',
-    api_key=settings.CMC_API_KEY
-)
